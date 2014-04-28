0acf3fb858071922676ccb1cc6696981411410ec
Santosh Ananthakrishnan <santosh@Santoshs-MacBook-Pro.local>
2014-04-28 15:11:20 -0400

    Minimize 4.py

--- Round1A-2014/4.py
+++ Round1A-2014/4.py
@@ -1,19 +1,7 @@
 import sys
-
-threshold = 400
-
 f = open(sys.argv[1])
 for t in range(1, int(f.readline())+1):
 	N = int(f.readline())
 	s = map(int, f.readline().split())
-	
-	too_close = 0
-	for index, i in enumerate(s):
-		diff = i - index
-		if diff <= 200 and diff >= 0:
-			too_close += 1
-
-	if too_close >= 200:
-		print 'Case #%d: BAD' % (t)
-	else:
-		print 'Case #%d: GOOD' % (t)
+	too_close = len(filter(lambda x: x <= 200 and x >= 0, [i-index for index, i in enumerate(s)]))
+	print 'Case #%d: %s' % (t, 'BAD' if too_close >= 200 else 'GOOD')
