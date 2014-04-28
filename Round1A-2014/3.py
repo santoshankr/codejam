b2a07011b489a986edcd2d0c012214e681238ce1
Santosh Ananthakrishnan <santosh@Santoshs-MacBook-Pro.local>
2014-04-28 15:13:23 -0400

    Renaming

--- Round1A-2014/3.py
+++ Round1A-2014/3.py
@@ -1,6 +1,7 @@
 import sys
-
 f = open(sys.argv[1])
 for t in range(1, int(f.readline())+1):
-	a = int(f.readline())
-	print 'Case #%d: %d' % (t, a)
+	N = int(f.readline())
+	s = map(int, f.readline().split())
+	too_close = len(filter(lambda x: x <= 200 and x >= 0, [i-index for index, i in enumerate(s)]))
+	print 'Case #%d: %s' % (t, 'BAD' if too_close >= 200 else 'GOOD')
--- Round1A-2014/4.py
+++
@@ -1,7 +0,0 @@
-import sys
-f = open(sys.argv[1])
-for t in range(1, int(f.readline())+1):
-	N = int(f.readline())
-	s = map(int, f.readline().split())
-	too_close = len(filter(lambda x: x <= 200 and x >= 0, [i-index for index, i in enumerate(s)]))
-	print 'Case #%d: %s' % (t, 'BAD' if too_close >= 200 else 'GOOD')
